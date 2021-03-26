de9c293fef5af0ce5c5391676996bf5dbe2e3d12
Elliot Blanchard <git@elliotblanchard.com>
2021-03-26 09:41:22 -0400

    Continued tutorial

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-const http = require('http')
+const express = require('express')
+const path = require('path')
 
-const hostname = '127.0.0.1'
-const port = 3000
+const app = express()
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain')
-    res.end('Hello World\n')
-})
+// Set static path
+app.use(express.static(path.join(__dirname,'public')))
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`)
+app.listen(3000, function() {
+    console.log('Server started on port 3000...')
 })
